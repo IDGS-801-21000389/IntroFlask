9f353160f9e0dd79be4ab6c7674b443582612feb
21000389-IDGS-801 <21000389@alumnos.utleon.edu.mx>
2026-01-26 18:55:19 -0600

    Macros

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    titulo ="Flask IDGS801"
+    lista = ["Juan", "Mario", "Pedro", "Dario"]
+    return render_template("index.html", titulo=titulo, lista=lista)
+
+@app.route('/alumnos')
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template("usuarios.html")
+
+@app.route('/operasBas', methods=['GET', 'POST'])
+def operasBas():
+    n1 = 0
+    n2 = 0
+    res = 0
+    if request.method == 'POST':
+        n1 = request.form.get('n1')
+        n2 = request.form.get('n2')
+        res = float (n1) +float(n2)
+    return render_template("operasBas.html", n1=n1, n2=n2, res=res)
+
+@app.route('/resultado', methods=['GET', 'POST'])
+def resultado():
+    n1 = request.form.get('n1')
+    n2 = request.form.get('n2')
+    tem = float (n1) +float(n2)
+    return f'La suma es: {tem}'
 
 @app.route('/hola')
 def hola():
@@ -16,11 +44,12 @@ def user(user):
 
 @app.route('/numero/<int:n>')
 def numero(n):
-    return "<h1>El numero es: {n}</h1>"
+    return f"<h1>El numero es: {n}</h1>"
 
+'''
 @app.route('/user/<int:id>/<string:username>')
 def username(id, username):
-    return "<h1>Hola, {username}, tu id es: {id}</h1>"
+    return f"<h1>Hola, {username}, tu id es: {id}</h1>"
 
 @app.route('/suma/<int:num1>/<int:num2')
 def sumas(num1, num2):
@@ -30,18 +59,17 @@ def sumas(num1, num2):
 @app.route('/default/<string:param')
 def default(param= "juanito"):
     return f"Hola, {param}"
-
+'''
 @app.route('/operas')
 def operas():
     return '''
-<form>
-<label for = "name"> Name: </label>
-<input type = "text" id="name" name="name" requiered>
-</br>
-<label for = "name"> apaterno: </label>
-<input type = "text" id="name" name="name" requiered>
+        <form>
+        <label for = "name"> Name: </label>
+        <input type = "text" id="name" name="name" requiered>
+        </br>
+        <label for = "name"> apaterno: </label>
+        <input type = "text" id="name" name="name" requiered>
 </form>
-
 '''
 
 if __name__ == "__main__":
